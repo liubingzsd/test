757c670946069aa7ba8851767a38fc523ec29b86
995813467 <995813467@qq.com>
2018-05-06 16:01:45 +0800

    add cholesky_solver

---
+++ test/cholesky_solver.h
@@ -0,0 +1,5 @@
+#ifndef _CHOLESKY_SOLVER_H_
+#define _CHOLESKY_SOLVER_H_
+#include <stdbool.h>
+bool cholesky_solver(double *a,  int n,int m, double *b);
+#endif
--- test/main.c
+++ test/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "gauss_jordan_solver.h"
-int main()
+#include "cholesky_solver.h"
+void test_gauss_jordan_solver()
 {
 	int i;
 	double a[4][4] = {
-	{ 0.2368,0.2471,0.2568,1.2671 },
+		{ 0.2368,0.2471,0.2568,1.2671 },
 	{ 0.1968,0.2071,1.2168,0.2271 },
 	{ 0.1581,1.1675,0.1768,0.1871 },
 	{ 1.1161,0.1254,0.1397,0.1490 }
@@ -14,5 +15,28 @@ int main()
 		for (i = 0; i <= 3; i++)
 			printf("x(%d)=%e\n", i, b[i]);
 	}
-
+}
+void test_cholesky_solver()
+{
+	int i;
+	 double a[4][4] = {
+		{ 5.0,7.0,6.0,5.0 },
+		{ 7.0,10.0,8.0,7.0 },
+		{ 6.0,8.0,10.0,9.0 },
+		{ 5.0,7.0,9.0,10.0 } 
+	 };
+	double d[4][2] = { 
+		{ 23.0,92.0 },
+		{ 32.0,128.0 },
+		{ 33.0,132.0 },
+		{ 31.0,124.0 } 
+	};
+	if (cholesky_solver(&a[0][0], 4, 2, &d[0][0]))
+		for (i = 0; i <= 3; i++)
+			printf("x(%d)=%13.7e,   %13.7e\n", i, d[i][0], d[i][1]);
+}
+int main()
+{
+	test_gauss_jordan_solver();
+	test_cholesky_solver();
 }
